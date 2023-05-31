21ab9ac572e0fcc95126094290004ea5e80d1910
Alfie Collins <alfiecollins44@gmail.com>
2023-05-31 22:33:32 +0100

    snake now grows upon eating and moves continuously

--- jsFiles/index.js
+++ jsFiles/index.js
@@ -109,35 +109,38 @@ if ( currentSnake.x < 0 || currentSnake.x > 681
 
 
  
-function detectCollision(food) {
-  return ((currentSnake.x < food.x + food.width) &&         // check left side of element 
-  (currentSnake.x + food.width > food.x) &&           // check right side
-  (currentSnake.y < food.y + food.height) &&         // check top side
-  (currentSnake.y + currentSnake.height > food.y));           // check bottom side
+ function detectCollision(food) {
+  return (
+    currentSnake.x < food.x + food.width && // check left side of element 
+    currentSnake.x + currentSnake.width > food.x && // check right side
+    currentSnake.y < food.y + food.height && // check top side
+    currentSnake.y + currentSnake.height > food.y // check bottom side
+  );
 }
 
-for(let i = 0; i<currentGame.food.length; i++) {
+for (let i = 0; i < currentGame.food.length; i++) {
   currentGame.food[i].drawFood();
 
+  if (detectCollision(currentGame.food[i])) {
+    // Generate a new piece of food
+    let randomFoodX = Math.floor(Math.random() * 680);
+    let randomFoodY = Math.floor(Math.random() * 480);
+    let randomFoodWidth = 20;
+    let randomFoodHeight = 20;
+    let newFood = new Food(randomFoodX, randomFoodY, randomFoodWidth, randomFoodHeight);
 
-if (detectCollision(currentGame.food[i])) {
-  //Draw a piece of food
-  let randomFoodX = Math.floor(Math.random() * 680);
-  let randomFoodY = Math.floor(Math.random() * 480);
-  let randomFoodWidth = 20;
-  let randomFoodHeight = 20;
-  let newFood = new Food(
-      randomFoodX, 
-      randomFoodY, 
-      randomFoodWidth, 
-      randomFoodHeight);
+    currentGame.food.splice(i, 1);
+    currentGame.food.push(newFood);
 
-      currentGame.food.splice(i, 1);
+    // Increase the score and update the display
+    currentGame.score++;
+    document.querySelector('.score').innerText = currentGame.score;
 
-  currentGame.food.push(newFood);
-  currentGame.score++ 
-  document.querySelector('.score').innerText = currentGame.score
-} 
+    // Make the snake grow by adding a new segment
+    let lastSegment = currentSnake.segments[currentSnake.segments.length - 1];
+    let newSegment = new SnakeSegment(lastSegment.x, lastSegment.y, currentSnake.width, currentSnake.height);
+    currentSnake.segments.push(newSegment);
+  }
 }
 
 function endGame(){
--- jsFiles/snake.js
+++ jsFiles/snake.js
@@ -1,38 +1,111 @@
 class Snake {
-    constructor(){
-      this.x = 231;
-      this.y = 300;
-      this.width = 20;
-      this.height = 20;
-    }
+  constructor() {
+    this.x = 231;
+    this.y = 300;
+    this.width = 20;
+    this.height = 20;
+    this.segments = [new SnakeSegment(this.x, this.y, this.width, this.height)];
+  }
 
-    drawSnake(){
-      ctx.fillStyle = 'blue';
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }   
-
-    moveSnake(keyCode){
-      ctx.clearRect(this.x, this.y, this.width, this.height);
-      switch(keyCode){
-        case 37: //left
-        if(this.x > -1){
-          this.x -= 10;
-        }
-          break;
-        case 39: //right
-        if (this.x < 685 ){
-          this.x += 10;
-        }
-          break;
-          case 38: //up
-          if(this.y>-1){
-             this.y -= 10;
-          }
-          break;
-          case 40: //down
-         if(this.y<481){
-             this.y += 10;
-         }
-      }
+  drawSnake() {
+    ctx.fillStyle = 'blue';
+    for (let segment of this.segments) {
+      ctx.fillRect(segment.x, segment.y, segment.width, segment.height);
     }
   }
+
+  moveSnake() {
+    ctx.clearRect(this.x, this.y, this.width, this.height);
+    let head = this.segments[0];
+
+    switch (currentDirection) {
+      case 37: //left
+        if (head.x > -1) {
+          this.x -= 10;
+        }
+        break;
+      case 39: //right
+        if (head.x < 685) {
+          this.x += 10;
+        }
+        break;
+      case 38: //up
+        if (head.y > -1) {
+          this.y -= 10;
+        }
+        break;
+      case 40: //down
+        if (head.y < 481) {
+          this.y += 10;
+        }
+        break;
+    }
+
+    // Move each segment of the snake
+    for (let i = this.segments.length - 1; i > 0; i--) {
+      this.segments[i].x = this.segments[i - 1].x;
+      this.segments[i].y = this.segments[i - 1].y;
+    }
+
+    // Update the position of the head
+    head.x = this.x;
+    head.y = this.y;
+  }
+}
+
+class SnakeSegment {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+}
+
+let currentDirection = null;
+let nextDirection = null;
+
+function handleKeyDown(event) {
+  const keyCode = event.keyCode;
+  if (keyCode >= 37 && keyCode <= 40) {
+    event.preventDefault();
+
+    if (!currentDirection) {
+      // Set the initial direction
+      currentDirection = keyCode;
+      moveSnake();
+    } else {
+      // Set the next direction
+      nextDirection = keyCode;
+    }
+  }
+}
+
+function handleKeyUp(event) {
+  const keyCode = event.keyCode;
+  if (keyCode === currentDirection) {
+    // Clear the next direction if the same arrow is released
+    nextDirection = null;
+  }
+}
+
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
+
+function moveSnake() {
+  setTimeout(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    currentSnake.moveSnake();
+
+    currentSnake.drawSnake();
+
+    if (nextDirection && nextDirection !== currentDirection) {
+      currentDirection = nextDirection;
+      nextDirection = null;
+    }
+
+    requestAnimationFrame(moveSnake);
+  }, 32); 
+}
+
